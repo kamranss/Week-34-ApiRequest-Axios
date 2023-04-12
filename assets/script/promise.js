aa77687f242948fc7b6e9f1482820f3a1e325f80
kamranss <kamranss@code.edu.az>
2023-04-12 09:37:09 +0400

    Refactor: get request created

---
+++ assets/script/promise.js
@@ -0,0 +1,10 @@
+;
+
+  axios.get('https://jsonplaceholder.typicode.com/users')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
