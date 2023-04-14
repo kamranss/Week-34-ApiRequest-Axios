e391e14518b9c43e5f6ff4ae3adb99b538045a2f
kamranss <kamranss@code.edu.az>
2023-04-14 14:33:30 +0400

    Refactor: Post method updated

--- assets/script/promise.js
+++ assets/script/promise.js
@@ -1,5 +1,6 @@
-;
+let registerBtn = document.querySelector(".register-btn");
 
+ function getDate(){
   axios.get('https://jsonplaceholder.typicode.com/users')
   .then((response) => {
     console.log(response.data);
@@ -8,3 +9,33 @@
     console.log(response.headers);
     console.log(response.config);
   });
+ }
+
+
+  registerBtn.addEventListener("click", function(){
+    let name = document.querySelector(".input-name");
+    let email = document.querySelector(".input-email");
+    
+    const data = {
+      postId: 1,
+      id: 6,
+      name: name.value,
+      email: email.value,
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec leo malesuada, lobortis nunc a, ultrices mi.'
+    };
+
+    postData(data);
+  })
+
+  
+// post data
+ function postData(data){
+    axios.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+ }
+
